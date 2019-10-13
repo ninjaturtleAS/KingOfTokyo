8b9ed8c6a5153b762b1529194d23b143035a16a8
si391rec <simon.rechermann@gmail.com>
2019-10-13 15:55:25 +0200

    Test1

--- src/main/scala/de/htwg/se/yourgame/KingOfTokyo.scala
+++ src/main/scala/de/htwg/se/yourgame/KingOfTokyo.scala
@@ -2,9 +2,9 @@ package de.htwg.se.yourgame
 
 import de.htwg.se.yourgame.model.Player
 
-object YourGame {
+object KingOfTokyo{
   def main(args: Array[String]): Unit = {
-    val student = Player("Your Name")
+    val student = Player("Simon")
     println("Hello, " + student.name)
   }
 }
