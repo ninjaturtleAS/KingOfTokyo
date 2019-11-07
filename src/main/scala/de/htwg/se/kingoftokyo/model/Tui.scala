688be1fee4085c5bf393e307ef37f9e76fadf733
ninjaturtleas <alexander.soehnlein@gmail.com>
2019-11-07 14:41:12 +0100

    printf

--- src/main/scala/de/htwg/se/kingoftokyo/model/Tui.scala
+++ src/main/scala/de/htwg/se/kingoftokyo/model/Tui.scala
@@ -6,8 +6,8 @@ class Tui {
     var player1 = Player("")
     var rightChoice = true
     while (rightChoice) {
-      println("Select your Monster!")
-      println("Type in 1 for King, 2 for Cyber Kitty, 3 for Ailionoid or 4 for Gigasaurus")
+      printf("Select your Monster!\n")
+      printf("Type in 1 for King, 2 for Cyber Kitty, 3 for Ailionoid or 4 for Gigasaurus\n")
       val scanner = new java.util.Scanner(System.in)
       if (scanner.hasNextInt) {
         val choice = scanner.nextInt()
@@ -16,13 +16,13 @@ class Tui {
           case 2 => rightChoice = false; player1 = Player("Cyber Kitty")
           case 3 => rightChoice = false; player1 = Player("Ailionoid")
           case 4 => rightChoice = false; player1 = Player("Gigasaurus")
-          case _ => println("wrong Number, try again")
+          case _ => printf("wrong Number, try again\n")
         }
       } else {
-        println("wrong input")
+        printf("wrong input\n")
       }
     }
-    println("You've chosen " + player1.info)
-    println("Welcome " + player1.name + " die to die")
+    printf("You've chosen " + player1.info + "\n")
+    printf("Welcome " + player1.name + " die to die\n")
   }
 }
