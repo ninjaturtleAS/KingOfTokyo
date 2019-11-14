4e07d9d9185f4a7011a5edde077afe7e1345e6ed
si391rec <simon.rechermann@gmail.com>
2019-11-14 21:54:07 +0100

    test2

--- src/main/scala/de/htwg/se/kingoftokyo/model/StatusMessageOld.scala
+++ src/main/scala/de/htwg/se/kingoftokyo/model/StatusMessageOld.scala
@@ -3,6 +3,6 @@ package de.htwg.se.kingoftokyo.model
 import de.htwg.se.kingoftokyo.model.State._
 
 class StatusMessageOld(message:String) {
-  override def toString(): String = message
+//  override def toString(): String = message
 
 }
--- src/test/scala/de/htwg/se/kingoftokyo/model/PlayersCreaterSpec.scala
+++ src/test/scala/de/htwg/se/kingoftokyo/model/PlayersCreaterSpec.scala
@@ -8,47 +8,25 @@ import Matchers._
 
 @RunWith(classOf[JUnitRunner])
 class PlayersCreaterSpec extends WordSpec with Matchers  {
-//  "A PlayersCreater at start" when {
-//    "new" should {
-//      val pc = PlayersCreator("Alex,Simon,Paul")
-//      "have a string representation" in {
-//        pc.toString should be("Alex")
-//      }
-//      "have heart" in {
-//        player.heart should be(initHeat)
-//      }
-//      "have energy" in {
-//        player.energy should be(initRest)
-//      }
-//      "have stars" in {
-//        player.stars should be(initRest)
-//      }
-//      "have a nice String representation" in {
-//        player.toString should be("Player(Alex,10,0,0)")
-//      }
-//
-//      "loose Heart to 9" in {
-//        player.looseHeart(one) should be(Player("Alex", initHeat -1, initRest, initRest))
-//      }
-//      "gain Heart not to 11" in {
-//        player.gainHeart(one) should be(Player("Alex", initHeat, initRest, initRest))
-//      }
-//      "gain Star to 1" in {
-//        player.gainStar(one) should be(Player("Alex", initHeat, initRest + 1, initRest))
-//      }
-//      "loose Star not to -1" in {
-//        player.looseStar(one) should be(Player("Alex", initHeat, initRest, initRest))
-//      }
-//      "gain Energy to 1" in {
-//        player.gainEnergy(one) should be(Player("Alex", initHeat, initRest, initRest + 1))
-//      }
-//      "spend Energy not to -1" in {
-//        player.spendEnergy(one) should be(Player("Alex", initHeat, initRest, initRest))
-//      }
-//      "have a nice Info String" in {
-//        player.info should be("Alex, Heart: 10, Stars: 0, Energy: 0")
-//      }
-//    }
-//  }
-
+  "A PlayersCreater at start" when {
+    "new" should {
+      val pc = PlayersCreator("Alex,Simon")
+      val strList = pc.toStringList
+      "have a String List representation" in {
+        strList should be(List("Alex", "Simon"))
+      }
+      "have a String of Names" in {
+        pc.stringOfNames should be(List("Alex,Simon"))
+      }
+      "have a length" in {
+        pc.numberOfPlayers should be(2)
+      }
+      "give random Players" in {
+        pc.getRandomPlayers(strList).toStringVector should contain allElementsOf Vector("Alex", "Simon")
+      }
+      "give random Players" in {
+        pc.getRandomPlayers(strList, new Players()).toStringVector should contain allElementsOf Vector("Alex", "Simon")
+      }
+    }
+  }
 }
