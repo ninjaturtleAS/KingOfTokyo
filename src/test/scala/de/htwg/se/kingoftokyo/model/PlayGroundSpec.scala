61feed4c308d17ec0d184ba0f228a4f1345a83fc
ninjaturtleas <alexander.soehnlein@gmail.com>
2019-11-15 08:55:00 +0100

    tests

--- src/test/scala/de/htwg/se/kingoftokyo/model/PlayGroundSpec.scala
+++ src/test/scala/de/htwg/se/kingoftokyo/model/PlayGroundSpec.scala
@@ -1,5 +1,14 @@
 package de.htwg.se.kingoftokyo.model
 
-class PlayGroundSpec {
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import Matchers._
+
+@RunWith(classOf[JUnitRunner])
+class PlayGroundSpec extends WordSpec with Matchers {
+
+
+  //incLapNr
 
 }
