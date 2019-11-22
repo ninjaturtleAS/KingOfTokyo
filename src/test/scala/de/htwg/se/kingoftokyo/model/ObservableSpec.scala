3dd27c2790130d8fcb01b177b102998408a2a3a2
ninjaturtleas <alexander.soehnlein@gmail.com>
2019-11-22 11:10:21 +0100

    observable

---
+++ src/test/scala/de/htwg/se/kingoftokyo/model/ObservableSpec.scala
@@ -0,0 +1,18 @@
+/*
+package de.htwg.se.kingoftokyo.util
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import Matchers._
+import de.htwg.se.kingoftokyo.model.PlayGround
+
+@RunWith(classOf[JUnitRunner])
+class ObservableSpec extends WordSpec with Matchers {
+
+  val observer1 = PlayGround;
+  val observer2 =
+
+
+}
+*/
