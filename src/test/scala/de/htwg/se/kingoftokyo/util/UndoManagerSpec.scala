636baef0617b50d5cf6aa195d5dd0d4d1e3c9783
si391rec <simon.rechermann@gmail.com>
2019-12-05 14:35:58 +0100

    testCreatePlayersCommand2

--- src/test/scala/de/htwg/se/kingoftokyo/util/UndoManagerSpec.scala
+++ src/test/scala/de/htwg/se/kingoftokyo/util/UndoManagerSpec.scala
@@ -1,5 +1,16 @@
 package de.htwg.se.kingoftokyo.util
 
-class UndoManagerSpec {
+import de.htwg.se.kingoftokyo.model._
+import org.scalatest._
 
+class UndoManagerSpec extends WordSpec with Matchers {
+
+  "UndoManager" when {
+    "new" should {
+      val udm = new UndoManager
+      "do Step" in {
+        udm.undoStep should be (udm.redoStep)
+      }
+    }
+  }
 }
