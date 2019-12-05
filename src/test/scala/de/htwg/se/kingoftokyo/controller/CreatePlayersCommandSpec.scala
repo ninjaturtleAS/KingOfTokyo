46acded21b8cfe62912e9d9256b15b63ed7727bd
si391rec <simon.rechermann@gmail.com>
2019-12-05 14:13:01 +0100

    testCreatePlayersCommand

--- src/test/scala/de/htwg/se/kingoftokyo/controller/CreatePlayersCommandSpec.scala
+++ src/test/scala/de/htwg/se/kingoftokyo/controller/CreatePlayersCommandSpec.scala
@@ -1,5 +1,40 @@
 package de.htwg.se.kingoftokyo.controller
 
-class CreatePlayersCommandSpec {
+import de.htwg.se.kingoftokyo.model._
+import org.scalatest._
 
+class CreatePlayersCommandSpec extends WordSpec with Matchers {
+
+  val lapNr = 0
+  val kot = 1
+  val initHeart =  10
+  val alex = Player("Alex")
+  val simon = Player("Simon")
+  val marco = Player("Marco")
+  val testString = "Alex, Simon, Marco"
+  val playersEmpty = Players(Vector())
+  val players = Players(Vector(alex, simon, marco))
+  val testResult =  RollResult(Vector(1, 2, 3, 4, 5, 6))
+  val badResult = RollResult(Vector(6, 6, 6, 6, 6, 6))
+
+  var playGroundWaitPlayers = PlayGround(players, lapNr, testResult, kot)
+  var playGroundEmpty = PlayGround(playersEmpty, lapNr, testResult, kot)
+
+  val controller = new Controller(playGroundWaitPlayers)
+  val controllerEmpty = new Controller(playGroundEmpty)
+  "CreatePlayersCommand" when {
+    "new" should {
+      val createPlayersCommand = new CreatePlayersCommand("Simon,Alex", controller)
+
+      "undo" in {
+        createPlayersCommand.undoStep
+        controller.state should equal (controllerEmpty.state)
+      }
+
+      "redo" in {
+        createPlayersCommand.redoStep
+        controller.state should equal (State.WaitFor1stThrow)
+      }
+    }
+  }
 }
