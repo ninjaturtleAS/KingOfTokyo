410feed80eef070cc1e1abef58f3db156dcec480
ninjaturtleas <alexander.soehnlein@gmail.com>
2019-12-06 09:56:13 +0100

    tests

--- src/test/scala/de/htwg/se/kingoftokyo/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/kingoftokyo/controller/ControllerSpec.scala
@@ -62,6 +62,7 @@ class ControllerSpec extends WordSpec with Matchers {
       }
 
       "filter throw results" in {
+        controller1.filterThrowResult("String") should be (controller1.playGround)
         controller1.filterThrowResult("0 1 2 3 4 5").rollResult.toString() should be ("1 2 3 Energy Heart Attack ")
         controller1.state = State.WaitFor1stThrow
         controller1.filterThrowResult("0 1 2 3 4 5").rollResult.toString() should be ("1 2 3 Energy Heart Attack ")
