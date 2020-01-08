267cec3aa694cf5f4119840e54127ae547073a24
si391rec <simon.rechermann@gmail.com>
2020-01-08 15:38:54 +0100

    FileIOnew

--- src/main/scala/de/htwg/se/kingoftokyo/model/fileIoComponent/fileIoJsonImpl/FileIO.java
+++
@@ -1,6 +0,0 @@
-package de.htwg.se.kingoftokyo.model.fileIoComponent.fileIoJsonImpl;
-
-public class FileIO {
-
-
-}
--- src/main/scala/de/htwg/se/kingoftokyo/model/fileIoComponent/fileIoXmlImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/kingoftokyo/model/fileIoComponent/fileIoXmlImpl/FileIO.scala
@@ -7,9 +7,63 @@ import net.codingwell.scalaguice.InjectorExtensions._
 import de.htwg.se.kingoftokyo.KingOfTokyoModule
 import de.htwg.se.kingoftokyo.model.fileIoComponent.FileIoInterface
 import de.htwg.se.kingoftokyo.model.playGroundComp.PlayGroundInterface
-import play.api.libs.json._
+
+import scala.xml.{NodeSeq, PrettyPrinter}
 
 
-class FileIO {
+class FileIO  extends FileIoInterface {
+  override def load: PlayGroundInterface = {
+    var grid: PlayGroundInterface = null
+    val file = scala.xml.XML.loadFile("playground.xml")
+    val sizeAttr = (file \\ "playground" \ "@size")
+    val size = sizeAttr.text.toInt
+    val injector = Guice.createInjector(new KingOfTokyoModule)
+    /*size match {
+      case 1 => grid = injector.instance[GridInterface](Names.named("tiny"))
+    }*/
+    val cellNodes = (file \\ "cell")
+    for (cell <- cellNodes) {
+      val row: Int = (cell \ "@row").text.toInt
+      val col: Int = (cell \ "@col").text.toInt
+      val value: Int = cell.text.trim.toInt
+      grid = grid.set(row, col, value)
+      val given = (cell \ "@given").text.toBoolean
+      val showCandidates = (cell \ "@showCandidates").text.toBoolean
+      if (given) grid = grid.setGiven(row, col, value)
+      if (showCandidates) grid = grid.setShowCandidates(row, col)
+    }
+    grid
+  }
+
+  def save(grid: GridInterface): Unit = saveString(grid)
+
+  def saveXML(grid: GridInterface): Unit = {
+    scala.xml.XML.save("grid.xml", gridToXml(grid))
+  }
+
+  def saveString(grid: GridInterface): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File("grid.xml"))
+    val prettyPrinter = new PrettyPrinter(120, 4)
+    val xml = prettyPrinter.format(gridToXml(grid))
+    pw.write(xml)
+    pw.close
+  }
+  def gridToXml(grid: GridInterface) = {
+    <grid size={ grid.size.toString }>
+      {
+      for {
+        row <- 0 until grid.size
+        col <- 0 until grid.size
+      } yield cellToXml(grid, row, col)
+      }
+    </grid>
+  }
+
+  def cellToXml(grid: GridInterface, row: Int, col: Int) = {
+    <cell row={ row.toString } col={ col.toString } given={ grid.cell(row, col).given.toString } isHighlighted={ grid.isHighlighted(row, col).toString } showCandidates={ grid.cell(row, col).showCandidates.toString }>
+      { grid.cell(row, col).value }
+    </cell>
+  }
 
 }
